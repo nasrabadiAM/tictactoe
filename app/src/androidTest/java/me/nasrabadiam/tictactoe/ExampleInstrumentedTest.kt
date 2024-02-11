ad86285edc00e60af59aaab2697deca90e804cfa
Ali Nasrabadi <nasrabadiam@gmail.com>
2024-02-11 10:32:40 +0330

    Update first ui test to get ready

--- app/src/androidTest/java/me/nasrabadiam/tictactoe/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/me/nasrabadiam/tictactoe/ExampleInstrumentedTest.kt
@@ -1,24 +1,22 @@
 package me.nasrabadiam.tictactoe
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("me.nasrabadiam.tictactoe", appContext.packageName)
+    fun greetingTest(): Unit = with(composeRule) {
+        setContent { Greeting(name = "Android") }
+        onNodeWithText("Hello Android!").assertExists()
     }
 }
