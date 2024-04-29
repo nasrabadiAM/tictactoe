75bbe9ced987d539c13ac3f9354fcca417f73a68
Ali Nasrabadi <nasrabadiam@gmail.com>
2024-04-29 11:47:11 +0330

    Add kotlin parcelize gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.parcelize) apply false
 }
 
 tasks.register<Detekt>("detektAll") {
