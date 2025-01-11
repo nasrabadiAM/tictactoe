d243c8fa2f0aca271cc13d4fae4384f3c7d24ef0
Ali Nasrabadi <nasrabadiam@gmail.com>
2025-01-11 09:46:51 +0330

    Fix lint issues

--- app/src/main/java/me/nasrabadiam/tictactoe/ui/SquareLayoutModifier.kt
+++ app/src/main/java/me/nasrabadiam/tictactoe/ui/SquareLayoutModifier.kt
@@ -1,20 +1,22 @@
 package me.nasrabadiam.tictactoe.ui
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.layout.layout
 
-fun Modifier.squareWrapContentLayout() = this then layout { measurable, constraints ->
-    // Determine the minimum dimension for square layout
-    val minDimension = minOf(constraints.maxWidth, constraints.maxHeight)
-    val placeable = measurable.measure(
-        constraints.copy(
-            maxWidth = minDimension,
-            maxHeight = minDimension,
-            minHeight = minDimension,
-            minWidth = minDimension
+internal fun Modifier.squareWrapContentLayout(): Modifier = composed {
+    layout { measurable, constraints ->
+        val minDimension = minOf(constraints.maxWidth, constraints.maxHeight)
+        val placeable = measurable.measure(
+            constraints.copy(
+                maxWidth = minDimension,
+                maxHeight = minDimension,
+                minHeight = minDimension,
+                minWidth = minDimension
+            )
         )
-    )
-    layout(minDimension, minDimension) {
-        placeable.place(x = 0, y = 0)
+        layout(minDimension, minDimension) {
+            placeable.place(x = 0, y = 0)
+        }
     }
 }
