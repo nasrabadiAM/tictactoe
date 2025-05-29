dda2c547b17798f9193d747d9da270bd5a8d5a94
Ali Nasrabadi <nasrabadiam@gmail.com>
2025-05-29 16:24:45 +0330

    Refactor code using claude

--- shared/src/commonMain/kotlin/me/nasrabadiam/tictactoe/game/GameUseCase.kt
+++ shared/src/commonMain/kotlin/me/nasrabadiam/tictactoe/game/GameUseCase.kt
@@ -3,12 +3,11 @@ package me.nasrabadiam.tictactoe.game
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import me.nasrabadiam.tictactoe.game.model.BOARD_SIZE
 import me.nasrabadiam.tictactoe.game.model.Cell
 import me.nasrabadiam.tictactoe.game.model.DEFAULT_BOARD_CELL_COUNT
 import me.nasrabadiam.tictactoe.game.model.GameResult
 import me.nasrabadiam.tictactoe.game.model.Player
-import me.nasrabadiam.tictactoe.game.model.utlis.getColumnIndex
-import me.nasrabadiam.tictactoe.game.model.utlis.getRowIndex
 import me.nasrabadiam.tictactoe.game.model.utlis.listOfEmptyCells
 import me.nasrabadiam.tictactoe.game.ui.DrawCount
 import me.nasrabadiam.tictactoe.game.ui.GameState
@@ -45,13 +44,20 @@ class GameUseCase(
     val currentPlayer: StateFlow<Player> = _currentPlayer
 
     fun clickOnCell(index: Int) {
-        if (gameResult.value != null) return
-        val copiedCells = cellsList
-        if (index >= copiedCells.size || copiedCells[index].value != null) return
-        copiedCells[index] = copiedCells[index].copy(value = currentPlayer.value)
-        _cells.update { copiedCells }
+        if (gameResult.value != null || !isValidMove(index)) return
+
+        _cells.update { cells ->
+            cells.mapIndexed { i, cell ->
+                if (i == index) cell.copy(value = currentPlayer.value) else cell
+            }
+        }
         changePlayerTurn()
         checkGameResultAndNotifyIfChanged()
+    }
+
+    private fun isValidMove(index: Int): Boolean {
+        val cells = _cells.value
+        return index in cells.indices && cells[index].value == null
     }
 
     fun restartGame() {
@@ -92,49 +98,32 @@ class GameUseCase(
     }
 
     private fun checkGameResultAndNotifyIfChanged() {
-        val winnerMap = mutableMapOf<String, Player?>()
-        cellsList.forEachIndexed { index, cell ->
-            val rowIndex = cellsList.getRowIndex(index)
-            val colIndex = cellsList.getColumnIndex(index)
-
-            // Row
-            val rowKey = getRowKey(rowIndex)
-            winnerMap.putInMap(rowKey, cell)
-
-            // Column
-            val colKey = getColKey(colIndex)
-            winnerMap.putInMap(colKey, cell)
-
-            // Left to right cross
-            getFirstCrossKey(rowIndex, colIndex)?.let { key ->
-                winnerMap.putInMap(key, cell)
-            }
-
-            // Right to left cross
-            getSecondCrossKey(rowIndex, colIndex)?.let { key ->
-                winnerMap.putInMap(key, cell)
+        checkForWinner()?.let { winner ->
+            updateWinnerScore(winner.player)
+            _gameResult.update { winner }
+            changePlayerTurn()
+        } ?: run {
+            if (hasNotAnyEmptyCell()) {
+                _drawCount.update { drawCount.value + 1 }
+                _gameResult.update { GameResult.Draw }
             }
         }
-        val nonNullWinnerMap = winnerMap.entries.filter { it.value != null }
+    }
 
-        if (nonNullWinnerMap.isNotEmpty()) {
-            val winner = requireNotNull(nonNullWinnerMap.first().value)
-            val winningKey = nonNullWinnerMap.first().key
-            updateWinnerScore(winner)
-            val orientation = getOrientation(winningKey)
-            val index = getWinningIndex(winningKey)
-            _gameResult.update {
-                GameResult.EndWithWinner(
-                    player = winner,
-                    winningOrientation = orientation,
-                    winningIndex = index
+    private fun checkForWinner(): GameResult.EndWithWinner? {
+        val cells = _cells.value
+        WINNING_PATTERNS.forEachIndexed { _, pattern ->
+            val values = pattern.cells.map { cells[it].value }
+            val player = values.firstOrNull()
+            if (player != null && values.all { it == player }) {
+                return GameResult.EndWithWinner(
+                    player = player,
+                    winningOrientation = pattern.orientation,
+                    winningIndex = pattern.index
                 )
             }
-            changePlayerTurn()
-        } else if (hasNotAnyEmptyCell()) {
-            _drawCount.update { drawCount.value + 1 }
-            _gameResult.update { GameResult.Draw }
         }
+        return null
     }
 
     private fun updateWinnerScore(winner: Player) {
@@ -149,74 +138,42 @@ class GameUseCase(
         return cellsList.any { it.value == null }.not()
     }
 
-    private fun MutableMap<String, Player?>.putInMap(
-        key: String,
-        cell: Cell
-    ) {
-        if (!containsKey(key)) {
-            this[key] = cell.value
-        }
-        if (this[key] != cell.value) {
-            this[key] = null
-        }
-    }
-
-    private fun getRowKey(rowIndex: Int): String {
-        return "row_$rowIndex"
-    }
-
-    private fun getColKey(colIndex: Int): String {
-        return "col_$colIndex"
-    }
-
-    private fun getFirstCrossKey(rowIndex: Int, colIndex: Int): String? {
-        return if (rowIndex == colIndex) {
-            "cross_0"
-        } else {
-            null
-        }
-    }
-
-    private fun getSecondCrossKey(rowIndex: Int, colIndex: Int): String? {
-        return if (
-            isSecondCrossCell(rowIndex, colIndex)
-        ) {
-            "cross_1"
-        } else {
-            null
-        }
-    }
-
-    private fun getWinningIndex(winningKey: String): Int {
-        val index = winningKey.split(WINNING_KEY_DELIMITER).getOrNull(1)
-        return index?.toInt()
-            ?: throw IllegalStateException(
-                "There is no winning index with this " +
-                    "(index value: $index), winningKey:$winningKey"
-            )
-    }
-
-    private fun getOrientation(winningKey: String): Orientation {
-        val orientation = winningKey.split(WINNING_KEY_DELIMITER).first()
-        return when (orientation) {
-            "cross" -> Orientation.CROSS
-            "row" -> Orientation.ROW
-            "col" -> Orientation.COLUMN
-            else -> throw IllegalStateException(
-                "There is no orientation with (name: $orientation)," +
-                    " winningKey:$winningKey"
-            )
-        }
-    }
-
-    private fun isSecondCrossCell(rowIndex: Int, colIndex: Int): Boolean {
-        return (rowIndex == 0 && colIndex == 2) ||
-            (rowIndex == 1 && colIndex == 1) ||
-            (rowIndex == 2 && colIndex == 0)
-    }
-
     companion object {
-        private const val WINNING_KEY_DELIMITER = "_"
+        private data class WinPattern(
+            val cells: List<Int>,
+            val orientation: Orientation,
+            val index: Int
+        )
+
+        private val WINNING_PATTERNS = buildList {
+            // Rows (0-2)
+            repeat(BOARD_SIZE) { row ->
+                val cells = (0 until BOARD_SIZE).map { col ->
+                    row * BOARD_SIZE + col
+                }
+                add(WinPattern(cells, Orientation.ROW, row))
+            }
+
+            // Columns (0-2)
+            repeat(BOARD_SIZE) { col ->
+                val cells = (0 until BOARD_SIZE).map { row ->
+                    row * BOARD_SIZE + col
+                }
+                add(WinPattern(cells, Orientation.COLUMN, col))
+            }
+
+            // Main diagonal (top-left to bottom-right)
+            val mainDiagonal = (0 until BOARD_SIZE).map { i ->
+                i * BOARD_SIZE + i
+            }
+            add(WinPattern(mainDiagonal, Orientation.CROSS, 0))
+
+            // Anti-diagonal (top-right to bottom-left)
+            val antiDiagonal = (0 until BOARD_SIZE).map { i ->
+                i * BOARD_SIZE + (BOARD_SIZE - 1 - i)
+            }
+            add(WinPattern(antiDiagonal, Orientation.CROSS, 1))
+        }
     }
 
     enum class Orientation {
--- shared/src/commonMain/kotlin/me/nasrabadiam/tictactoe/game/model/Constants.kt
+++ shared/src/commonMain/kotlin/me/nasrabadiam/tictactoe/game/model/Constants.kt
@@ -1,3 +1,4 @@
 package me.nasrabadiam.tictactoe.game.model
 
 const val DEFAULT_BOARD_CELL_COUNT = 9
+const val BOARD_SIZE = 3
